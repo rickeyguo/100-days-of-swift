8f3d68c638e88a61d145ec0f11eb32fdcb4c4d5f
Rickey Guo <32823151+rickeyguo@users.noreply.github.com>
2020-10-04 21:39:50 -0400

    day 1 complete

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "bye, playground"
+
+var age = 38
+var population = 8_000_000
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141
+
+var awesome = true
+
+var score = 85
+
+var scoreString = "your score was \(score)"
+
+let taylor = "swift"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
