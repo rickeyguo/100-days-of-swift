0e407e3388fd4810fa31bc34e980ede7132886b5
Rickey Guo <32823151+rickeyguo@users.noreply.github.com>
2020-10-05 23:58:45 -0400

    day 2 complete

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+// Day 1
 var str = "Hello, playground"
 str = "bye, playground"
 
@@ -32,3 +33,52 @@ let album: String = "Reputation"
 let year: Int = 1989
 let height: Double = 1.78
 let taylorRocks: Bool = true
+
+// Day 2
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+
+beatles[1]
+
+let colors = Set(["red", "green", "blue"])
+
+
+var name = (first: "Taylor", last: "Swift")
+
+name.0
+
+name.first
+
+let address = (house: 555, street: "Taylor Swift Avenue", city: "Nashville")
+
+let set = Set(["aardvark", "astronaut", "azalea"])
+
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+
+let heights = [
+    "Taylor Swift": 1.78,
+    "Ed Sheeran": 1.73
+]
+
+var teams = [String: String]()
+
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+let talking = Activity.talking(topic: "football")
+
+enum Planet: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
+
+let earth = Planet(rawValue: 3)
