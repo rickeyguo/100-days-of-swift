032be2e3aeb2acf0684403ce26c2abc23276ce6b
Rickey Guo <32823151+rickeyguo@users.noreply.github.com>
2020-10-14 19:15:30 -0400

    day 11 protocols complete

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -264,3 +264,29 @@ class Dog {
 }
 
 let poppy = Dog(name: "Poppy", breed: "Poodle")
+
+protocol Identifiable {
+    var id: String { get set }
+}
+
+struct UserWithProtocol: Identifiable {
+    var id: String
+}
+
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
+
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Employee: Payable, NeedsTraining, HasVacation { }
