5d50319c4ad15e20f2201908a881bc5433836cf3
Rickey Guo <32823151+rickeyguo@users.noreply.github.com>
2020-10-17 23:14:44 -0400

    day 14 complete

---
+++ consolidation.playground/Contents.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+func getHaterStatus(weather: String) -> String? {
+    if weather == "sunny" {
+        return nil
+    } else {
+        return "Hate"
+    }
+}
+
+func takeHaterAction(status: String) {
+    if status == "Hate" {
+        print("Hating")
+    }
+}
+
+if let haterStatus = getHaterStatus(weather: "rainy") {
+    takeHaterAction(status: haterStatus)
+}
+
+func albumReleased(year: Int) -> String? {
+    switch year {
+    case 2006: return "Taylor Swift"
+    case 2008: return "Fearless"
+    case 2010: return "Speak Now"
+    case 2012: return "Red"
+    case 2014: return "1989"
+    default: return nil
+    }
+}
+
+let album = albumReleased(year: 2006) ?? "unknown"
+print("The album is \(album)")
+
+struct Person {
+    var clothes: String
+    var shoes: String
+}
+
+class Singer {
+    var name: String
+    var age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+
+    func sing() {
+        print("La la la la")
+    }
+}
+
+class CountrySinger: Singer {
+    override func sing() {
+        print("Trucks, guitars, and liquor")
+    }
+}
+
+var taylor = CountrySinger(name: "Taylor", age: 25)
+taylor.sing()
